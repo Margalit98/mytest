139329b8e5405e26f58edf6a7ea51fba6a2810c1
Margalit98 <142243194+Margalit98@users.noreply.github.com>
2023-08-14 18:32:47 +0300

    Add files via upload

---
+++ MyTest/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyTest;
+
+Console.WriteLine("My test");
+Student student = new Student("Margalit Mizrachi", "Ben ptachya 22", "Bni brak", "develop software");
+Console.WriteLine(student.ToString());
+
+Hello hello = new Hello();
---
+++ MyTest/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTest
+{
+    public class Student:Person
+    {
+        public string Subject { get; set; }
+        public Student() { }
+        public Student(string fullname, string addres, string city, string subject) {
+            FullName = fullname;
+            Address = addres;
+            City = city;
+            Subject = subject;
+        }
+        public override string ToString()
+        {
+            return "My name is "+FullName+" and I live in "+Address+", "+City+".\nI am studing is "+Subject;
+        }
+    }
+}
